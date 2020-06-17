6db683e857df91cf328f7f8c4f3a70c05283ef42
Gladston Aristoverne <gladston.aristoverne.ga@gmail.com>
2020-06-17 12:15:26 -0400

    add books

--- js/main.js
+++ js/main.js
@@ -1,6 +1,70 @@
+const table = document.querySelector('.table');
+const addBooks = document.querySelector('.addBooks');
+const formAddBooks = document.querySelector('.formAddBooks');
 
-let table = document.querySelector('table');
-let table2 = document.querySelector('#tableauLivres');
-let table3 = document.querySelector('.monTableau');
+let livre1 = {
+    name: 'The book one',
+    auteur: 'Steve Balalou',
+    Pages: 500
+};
+
+let livre2 = {
+    name: 'The book one',
+    auteur: 'Steve Balalou',
+    Pages: 500
+};
+
+let livre3 = {
+    name: 'The book one',
+    auteur: 'Steve Balalou',
+    Pages: 500
+};
+
+const allBooks = [livre1, livre2, livre3];
+
+for (i = 0; i <= allBooks.length; i++) {
+    let add = document.createElement("tr");
+
+    add.innerHTML += `
+        <td> ${allBooks[i].name}</td>
+        <td> ${allBooks[i].auteur}</td>
+        <td> ${allBooks[i].Pages}</td>
+        <td><button>Modifier</button></td>
+        <td><button>Suprimer</button></td>
+        `
+    table.appendChild(add);
+}
+
+addBooks.addEventListener('click', () => {
+    let createForm = document.createElement('form');
+    createForm.innerHTML = `
+    <div>
+        <label for="Name"> Titre du livre</label>
+        <input type="text">
+    </div>
+    <div>
+        <label for="author"> Auteur</label>
+        <input type="text">
+    </div>
+    <div>
+        <label for="pages"> Nombre de pages</label>
+        <input type="number">
+    </div>
+    <div>
+        <label for="pages"> Nombre de pages</label>
+        <textarea name="description" id="description" cols="30" rows="5"></textarea>
+    </div>
+    `
+    formAddBooks.appendChild(createForm);
+});
+
+
+
+
+
+
+
+
+
 
 
