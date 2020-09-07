b4bd607f70444d9300b88f5d05e1427b857aef24
Gladston Aristoverne <gladston.aristoverne.ga@gmail.com>
2020-09-07 00:49:11 -0400

    delete

--- routes/auteurs.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const auteurController = require('../controllers/auteurs');
-
-router.get("/auteurs/:id", auteurController.auteur_affichage );
-
-
-module.exports = router;
--- routes/home.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const homeController = require('../controllers/home');
-
-router.get('/', homeController.homeLogin)
-router.get('/log/register', homeController.register)
-
-module.exports = router;
