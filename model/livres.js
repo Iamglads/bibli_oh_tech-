02c4e7c9e13e26ab3b6f9efa40896375f566a798
Gladston Aristoverne <gladston.aristoverne.ga@gmail.com>
2020-09-07 00:53:15 -0400

    model/livres.js

---
+++ model/livres.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const livresSchema = mongoose.Schema({
+    _id : mongoose.Schema.Types.ObjectId,
+    nom: String,
+    auteur: String,
+    pages: Number,
+    description: String,
+    image: String
+})
+
+const livresModel = mongoose.model("livres", livresSchema);
+
+module.exports = livresModel;
